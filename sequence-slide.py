30e9be7e5cb4480180260f17d0aecbb8967790cb
bikait <bikait@pop-os.localdomain>
2022-07-11 23:01:46 +0200

    VRP introduction

--- sequence-slide.py
+++ sequence-slide.py
@@ -17,7 +17,9 @@ class VRPIntro(Slide):
 
     def construct(self):
         # introduction
-        text = MarkupText(f"From <span fgcolor='{BLUE}'>TSP</span>\nto Vehicle Routing Problems (<span fgcolor='{BLUE}'>VRP</span>)")
+        text_tsp_to_vrp = MarkupText(f"From <span fgcolor='{BLUE}'>TSP</span>\nto Vehicle Routing Problems (<span fgcolor='{BLUE}'>VRP</span>)")
+        text_successor = MarkupText("Successor model", color=BLUE)
+        text_group = VGroup(text_tsp_to_vrp, text_successor).arrange(DOWN, center=False, aligned_edge=LEFT).to_corner(UP + LEFT)
         coords = [
             np.array([-4.25, 0.75, 0]),
             np.array([-1.75, 2.75, 0]),
@@ -27,18 +29,102 @@ class VRPIntro(Slide):
             np.array([1.75, 1, 0]),
             np.array([3.25, -0.75, 0]),
             np.array([2.5, -2, 0]),
-            np.array([4.75, 1, 0]),
+            np.array([1.5, 0, 0]),
         ]
         dots = [Dot(i) for i in coords]
         dot_group = VGroup(*dots)
-        VGroup(text, dot_group).arrange(DOWN)
-        self.play(FadeIn(text))
+        VGroup(text_group, dot_group).arrange(DOWN)
+        self.play(FadeIn(text_tsp_to_vrp))
         self.pause()
         self.play(*[FadeIn(dot) for dot in dots])
         self.pause()
-        self.wait()
+        # example of path between the nodes
+        ordering = [0, 1, 4, 5, 8, 6, 7, 3, 2]
+        arrows = [Arrow(dots[i], dots[j], color=BLUE) for i, j in zip(ordering, ordering[1:] + [0])]
+        arrows_animations = [GrowArrow(arrow) for arrow in arrows]
+        self.play(AnimationGroup(*arrows_animations, lag_ratio=.2))
+        self.pause()
+        # remove the path and add new path for 2 vehicles
+        ordering_1 = [0, 1, 4, 5, 2]
+        ordering_2 = [0, 3, 7, 6, 8]
+        self.play(AnimationGroup(*[FadeOut(arrow) for arrow in arrows]))
+        arrows_1 = [Arrow(dots[i], dots[j], color=PURPLE) for i, j in zip(ordering_1, ordering_1[1:] + [0])]
+        arrows_2 = [Arrow(dots[i], dots[j], color=TEAL) for i, j in zip(ordering_2, ordering_2[1:] + [0])]
+        self.play(AnimationGroup(*[GrowArrow(arrow) for arrow in arrows_1], lag_ratio=.2))
+        self.play(AnimationGroup(*[GrowArrow(arrow) for arrow in arrows_2], lag_ratio=.2))
+        self.pause()
+
+        # add time window on top of nodes: values between [0..99]
+        tws = [
+            [0, 99],
+            [5, 15],
+            [80, 99],
+            [0, 30],
+            [25, 60],
+            [70, 80],
+            [60, 70],
+            [40, 50],
+            [60, 99],
+        ]
+        # a time window is simply a red rectangle for the invalid time, a green for the valid time and a red again
+        tw_animation_list = []
+        tw_group_list = []
+        height = 0.1
+        for i, dot in enumerate(dots):
+            tw = tws[i]
+            lengths = ([0] + tw + [99])
+            l = [(j-i)/100 for i, j in zip(lengths, lengths[1:])]
+            origin_y = dot.get_y() + 10
+            origin_x = dot.get_x()
+            before = Rectangle(color=RED, fill_opacity=1, width=l[0], height=height, stroke_width=0)
+            during = Rectangle(color=GREEN, fill_opacity=1, width=l[1], height=height, stroke_width=0)
+            after = Rectangle(color=RED, fill_opacity=1, width=l[2], height=height, stroke_width=0)
+            group = VGroup(before, during, after).arrange(RIGHT, buff=0).next_to(dot, UP)
+            tw_group_list.append(group)
+            animation = GrowFromPoint(group, dot)
+            tw_animation_list.append(animation)
+        self.play(AnimationGroup(*tw_animation_list))
+        self.pause()
+        self.play(AnimationGroup(*[ShrinkToCenter(tw) for tw in tw_group_list]))
+        self.pause()
+
+
+        # transform the nodes into arrows pointing upwards or downwards, if pickup or drop
+        self.play(AnimationGroup(*[FadeOut(dot) for dot in dots[1:]]))
+        pickups = [1, 4, 3, 6]
+        drops = [2, 5, 7, 8]
+        request_colors = [GREEN, BLUE, RED, GOLD]
+        images_height = height * 4
+        pickup_img = [
+            ImageMobject("res/restaurant_red.png").scale_to_fit_height(images_height),
+            ImageMobject("res/restaurant_green.png").scale_to_fit_height(images_height),
+            ImageMobject("res/restaurant_blue.png").scale_to_fit_height(images_height),
+            ImageMobject("res/restaurant_gold.png").scale_to_fit_height(images_height),
+        ]
+        drop_img = [
+            ImageMobject("res/house_red.png").scale_to_fit_height(images_height),
+            ImageMobject("res/house_green.png").scale_to_fit_height(images_height),
+            ImageMobject("res/house_blue.png").scale_to_fit_height(images_height),
+            ImageMobject("res/house_gold.png").scale_to_fit_height(images_height),
+        ]
+        for i, pickup in enumerate(pickups):
+            img = pickup_img[i]
+            img.move_to(dots[pickup])
+        for i, drop in enumerate(drops):
+            img = drop_img[i]
+            img.move_to(dots[drop])
+        self.play(AnimationGroup(*[FadeIn(img) for img in pickup_img + drop_img]))
+
+        self.pause()
+        self.play(AnimationGroup(*[FadeOut(img) for img in pickup_img + drop_img]))
+        self.play(AnimationGroup(*[FadeIn(dot) for dot in dots[1:]]))
+        self.pause()
 
         # successor model
+        self.play(FadeIn(text_successor))
+        self.pause()
+        self.wait()
+
 
 
 class SequenceOtherWork(Slide):
@@ -93,6 +179,7 @@ class Sequences(Slide):
         animations = [GrowArrow(succ) for succ in successors]
         self.play(AnimationGroup(*animations, lag_ratio=0.25))
         self.pause()
+
 
         # excluded nodes
         self.play(excluded_group.animate.set_color(EXCLUDED))
